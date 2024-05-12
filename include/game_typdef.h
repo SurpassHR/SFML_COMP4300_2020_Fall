2dc20bed671fa9f83c24da146777befd8cbbb734
hurui <hu.rui0530@gmail.com>
2024-05-12 18:43:14 +0800

    typdef reform

--- include/game_typdef.h
+++ include/game_typdef.h
@@ -1,14 +1,21 @@
 #ifndef GAME_TYPDEF_H
 #define GAME_TYPDEF_H
 
-#define GAME_I8 char
-#define GAME_U8 unsigned char
-#define GAME_I16 short
-#define GAME_U16 unsigned short
-#define GAME_I32 int
-#define GAME_U32 unsigned int
+#include <SFML/Graphics.hpp>
+
+#define GAME_I8 sf::Int8
+#define GAME_U8 sf::Uint8
+#define GAME_I16 sf::Int16
+#define GAME_U16 sf::Uint16
+#define GAME_I32 sf::Int32
+#define GAME_U32 sf::Uint32
 #define GAME_F32 float
 #define GAME_F64 double
+
+#define GAME_VEC_U16 sf::Vector2u;
+#define GAME_VEC_F32 sf::Vector2f;
+#define GAME_VEC_F64 sf::Vector2d;
+
 #define GAME_STR std::string
 
 #define GAME_OK 0
